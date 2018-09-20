1f600dc60fcace1e71b76e5057705b07dd81cf54
Eryl <eryl.murphy@gmail.com>
2018-09-20 16:28:03 -0400

    working on about button

--- src/Components/Pages/Homepage.js
+++ src/Components/Pages/Homepage.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import About from './About.js';
 
 class Homepage extends Component {
   render() {
     return (
+      <Router>
       <div className="Homepage">
         <h3>Welcome</h3>
         <h1>GREAT NORTHERN <br/> WIRELESS ASSOCIATION </h1>
+          <Link to="/About">
         <button className="about-button">ABOUT US</button>
+        </Link>
+          <Route path="/About" exact component={About} />
 
       </div>
+      </Router>
     );
   }
 }
