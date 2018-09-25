e68c2af14097fb85537c187506ebbfd878beea39
Eryl <eryl.murphy@gmail.com>
2018-09-25 18:04:20 -0400

    about hyperlink and trying to have main fill space

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header.js';
 import Homepage from './Components/Pages/Homepage.js';
 import Events from './Components/Pages/Events.js';
 import Philanthropy from './Components/Pages/Philanthropy.js';
-import Member from './Components/Pages/Member.js';
 import About from './Components/Pages/About.js';
 import Connect from './Components/Pages/Connect.js';
 import Gallery from './Components/Pages/Gallery.js';
@@ -17,7 +15,7 @@ class App extends Component {
     return (
       <Router>
       <div className="App">
-       {/* <Header /> */}
+      <div className="container">
         <nav>
             <Link to="/">Home</Link> 
             <Link to="/Events">Events</Link> 
@@ -27,17 +25,20 @@ class App extends Component {
             </a>
             <Link to="/About">About</Link>
             <Link to="/Philanthropy">Philanthropy</Link>
-            <Link to="/Member">Member</Link>
             <Link to="/Connect">Connect</Link>
         </nav>
+        <main>
           <Route path="/" exact component={Homepage} />
           <Route path="/Events" exact component={Events} />
           <Route path="/Gallery" exact component={Gallery} />
           <Route path="/About" exact component={About} />
           <Route path="/Philanthropy" exact component={Philanthropy} />
-          <Route path="/Member" exact component={Member} />
           <Route path="/Connect" exact component={Connect} />
+            </main>
+            <footer>
        <Footer />
+          </footer>
+          </div>
       </div>
       </Router>
     );
--- src/Components/Pages/Homepage.js
+++ src/Components/Pages/Homepage.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import About from './About.js';
 
 export default class Homepage extends Component {
-  
   render() {
     return (
-      <Router>
       <div className="Homepage">
         <h3>Welcome</h3>
         <h1>GREAT NORTHERN <br/> WIRELESS ASSOCIATION </h1>
-          <Link to="/About">
-        <button className="about-button">ABOUT US</button>
-        </Link>
-          <Route path="/About" exact component={About} />
-
+        <a className="about-button" href="/About">ABOUT US</a>
       </div>
-      </Router>
     );
   }
 }
