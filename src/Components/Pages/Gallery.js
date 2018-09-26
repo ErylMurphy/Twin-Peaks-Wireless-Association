587b6f51e90a330097d7b4fa1d5bc8bc6433d3ea
Eryl <eryl.murphy@gmail.com>
2018-09-26 18:17:56 -0400

    working on gallery

--- src/Components/Pages/Gallery.js
+++ src/Components/Pages/Gallery.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import ImageGallery from 'react-image-gallery';
 
 export default class Gallery extends Component {
   render() {
+    const images = [
+      {
+        original: "https://www.dailyscandinavian.com/wp-content/uploads/2016/12/231216-tomten.jpg", 
+      },
+      {
+        original: "https://images-na.ssl-images-amazon.com/images/I/51Z6H8spdXL.jpg",
+      }
+    ];
     return (
       <div className="Gallery">
         <h1>Gallery</h1>
-        <img className="mySlides" src="https://www.dailyscandinavian.com/wp-content/uploads/2016/12/231216-tomten.jpg"/>
+        <ImageGallery items={images} />
       </div>
     );
   }
