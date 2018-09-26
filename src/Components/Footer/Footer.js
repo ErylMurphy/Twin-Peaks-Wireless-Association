a2c9b5fdc8fcea2bf0fbe49a5daa22e21fb98516
Eryl <eryl.murphy@gmail.com>
2018-09-26 17:06:54 -0400

    working on about

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -7,8 +7,10 @@ export default class Footer extends Component {
   render() {
     return (
       <div className="footer">
+      <div className="footer-nav">
         <a href="/About">ABOUT</a>
         <a href="/Connect">CONNECT</a>
+        </div>
         <img className="footer-logo" src="logo.png" />
       </div>
     );
--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -5,8 +5,16 @@ export default class About extends Component {
     return (
       <div className="about">
         <h1>About Us</h1>
-        <h3>
+        <h2 className="mission-statement-title">Mission Statement</h2>
+        <h3 className="mission-statement-text">
 The Colorado Wireless Association is a non-profit professional association dedicated to being the voice of the wireless industry in the Centennial State. Our members represent wireless carriers, tower companies, network developers, and vendors supporting the industry. Our Mission is to promote the creative talent and energy of our membership through education and relationship building within the wireless industry and with our Colorado communities.  </h3>
+<h2 className="board-title">Board of Trustees</h2>
+<ul className="board-members">
+  <li>Tom Murphy</li>
+  <li>Tom Bombadil</li>
+  <li>Tom Jones</li>
+  <li>Tom Ten</li>
+  </ul> 
       </div>
     );
   }
