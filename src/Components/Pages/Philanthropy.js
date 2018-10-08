3146573bf523e4d460317b24f015c4f8a2485352
Eryl <eryl.murphy@gmail.com>
2018-10-08 16:33:14 -0400

    images on imgur

--- src/Components/Pages/Philanthropy.js
+++ src/Components/Pages/Philanthropy.js
@@ -6,7 +6,7 @@ export default class Philanthropy extends Component {
       <div className="Philanthropy">
       <h1>Philanthropy</h1>
         <a href="https://secure2.convio.net/nrcst/site/SPageServer/;jsessionid=00000000.app274a?NONCE_TOKEN=AF6F724FF1F07D39FF4B079AB0071207&pagename=pwna_donate"> <img className="pwna"src="http://blog.nativepartnership.org/wp-content/uploads/2015/08/PWNA_Logo_PMS.jpg"/></a>
-        <a href="https://donate.nationalparks.org/page/10780/donate/1?gclid=CjwKCAjworfdBRA7EiwAKX9HeNjGzIhFa-68u2Nal_zFqitCbHpz7HY71-Kbqx0rDu6pZ_3l6TW7eRoCqCAQAvD_BwE"> <img className="pwna" src="http://photos.prnewswire.com/prnfull/20140324/DC89075LOGO?max=200"/></a>
+        <a href="https://donate.nationalparks.org/page/10780/donate/1?gclid=CjwKCAjworfdBRA7EiwAKX9HeNjGzIhFa-68u2Nal_zFqitCbHpz7HY71-Kbqx0rDu6pZ_3l6TW7eRoCqCAQAvD_BwE"> <img className="pwna" src="https://www.thewaltdisneycompany.com/wp-content/uploads/2014/04/unnamed-2.jpg"/></a>
       </div>
     );
   }
