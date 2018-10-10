e457ff6e03ef18218749b166e53b230c3886286e
Eryl <eryl.murphy@gmail.com>
2018-10-10 17:14:05 -0400

    burger

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -1,11 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Homepage from '../Pages/Homepage.js';
-import Events from '../Pages/Events.js';
-import Philanthropy from '../Pages/Philanthropy.js';
-import About from '../Pages/About.js';
-import Connect from '../Pages/Connect.js';
-import Gallery from '../Pages/Gallery.js';
+
 
 class Burger extends Component {
   refreshPage() {
@@ -19,20 +13,17 @@ class Burger extends Component {
           <span></span>
           <span></span>
           <span></span>
-          <Router>
-            <ul id="burger">
-              <a href="/"><li>HOME</li></a>
-              <a href="/Events"><li>EVENTS</li></a>
-              <a href="/Gallery"><li>GALLERY</li></a>
-              <a href="/About">ABOUT <li>US</li></a>
-              <a href="/Philanthropy"><li>PHILANTHROPY</li></a>
-              <a href="/Connect"><li>CONNECT</li></a>
-            </ul>
-          </Router>
+          <ul id="burger">
+            <a href="/"><li>HOME</li></a>
+            <a href="/Events"><li>EVENTS</li></a>
+            <a href="/Gallery"><li>GALLERY</li></a>
+            <a href="/About"><li>ABOUT US</li></a>
+            <a href="/Philanthropy"><li>PHILANTHROPY</li></a>
+            <a href="/Connect"><li>CONNECT</li></a>
+          </ul>
         </div>
       </div>
     )
-
   }
 }
 
